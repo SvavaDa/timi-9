1d473042b247cef20eb3739c2025c06d119f0478
SvavaDa <svavada@gmail.com>
2025-10-15 19:25:50 +0000

    Bætur

---
+++ array.js
@@ -0,0 +1,39 @@
+let pets = ["dog", "cat", "hamster", "cat"];
+console.log(pets);
+console.log(pets.length);
+console.log(pets[1]);
+// console.log(pets);
+
+console.log(pets.includes("cat"));
+console.log(pets.indexOf("cat"));
+if (pets[1] === "cat") {
+}
+
+let nums = [1, 2, 3, 4, 5, 6];
+console.log(nums);
+
+//notum function var1,var2)
+//notum[]þegar notumst with ARRAY eða til að komast í value innan ARRAY eða Object
+//[] =ARRAY
+//{} =Object
+// sker út value í lista frá index1 að index 2
+let index1 = 2;
+let index2 = 5;
+// let sliced = nums.slice(2, 5);
+let sliced = nums.slice(index1, index2);
+console.log(sliced);
+
+// let loggedIN= false;
+// const login(){
+//     const usernmae = getElemetbyId("usernameInput")
+//     const passwors = getElemetbyId("passwordInput")
+//     if(usernmae ==DBusername && password ===DBpassword) )
+// }
+
+// let num = 22;
+// let num2 = "22";
+
+// console.log(num === num2);
+// =gefa/skrifa value
+//hálfur amanburdur
+//"Absoulte smanburður "
