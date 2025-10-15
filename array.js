13aad63d3e0c616f700641c559aa3f856924f6fd
SvavaDa <svavada@gmail.com>
2025-10-15 20:52:07 +0000

    Bætur3

--- array.js
+++ array.js
@@ -24,6 +24,45 @@ let sliced = nums.slice(index1, index2);
 console.log(sliced);
 console.log(nums);
 
+// nums.splice(2, 2, 99, 100);
+console.log(nums);
+
+//let spliced = nums.splice(1, 3, 99, 100); //breytir upp haf gögnum
+//splice (start, deleteCount, hvað kemur í staðinn)
+let spliced = nums.splice(1, 3); //breytir upp haf gögnum
+console.log(spliced);
+console.log(nums); //sýnir heildina eftir að hafa sett saman array með splice
+
+let letters = ["c", "b", "a", "g", "z"];
+let oneTime = [...letters];
+console.log(letters);
+//sorter eftir starfróðsöð
+letters.sort();
+//spegalar línuna
+letters.reverse();
+console.log(letters);
+
+console.log(oneTime);
+
+//map, filter, reduce-þetta helta
+
+let numbers = [1, 2, 3, 4, 5];
+// numbers [0]*2
+
+//map mappar yfir hvert item í arrayog framkvæmir skipun
+//filter mappar yfir hvert item í array og uppfyllit kröfur
+//reduce byrjar með einhverja grunn og bætir við hverjum item ofan á hann
+let dubled = numbers.map((n) => n * 2);
+console.log(numbers);
+console.log(dubled);
+let even = numbers.filter((n) => n % 2 === 0);
+console.log(even);
+let sum = numbers.reduce((sum, numbers) => sum + numbers);
+console.log(sum);
+// constmultiply(numbers){
+// for(let i=0, i<numbers.length;i++){ numbers[i]*2}
+// return numbers
+
 // let loggedIN= false;
 // const login(){
 //     const usernmae = getElemetbyId("usernameInput")
